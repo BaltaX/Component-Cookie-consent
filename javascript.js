04e376ad32f8cb4967cbc66636c710be200a961e
BaltaX <kristian.sundstrom@agrifodd.lu.se>
2019-03-22 15:17:27 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
 $("#accept-cookies").click(function () {
     $("#cookie-consent").css("display", "none");
 });
-document.cookie = 'cookie-consent=1';
 
-console.log(getCookie('cookie-consent'));
+//Does cookie exist
+if (getCookie("cookie-consent") == 1 {
+        //Remove display of box
+        alert("Hittade cookie")
+    } else {
+        alert("Hittade inte cookie")
+    }
 
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-}
+
+
+
+
+
+
+    //document.cookie = 'cookie-consent=1';
+
+    //console.log(getCookie('cookie-consent'));
+
+    function getCookie(name) {
+        var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+        return v ? v[2] : null;
+    }
