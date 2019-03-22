7afaa2e434e799f33a15abf633615b4494dc2fc7
BaltaX <kristian.sundstrom@agrifodd.lu.se>
2019-03-22 14:52:13 +0100

    Added getcookie functionality

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
 $("#accept-cookies").click(function () {
     $("#cookie-consent").css("display", "none");
 });
+document.cookie = 'cookie-consent=1';
+
+console.log(setCookie('cookie-consent'));
+
+function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
