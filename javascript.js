37b2bc46912a198e3322541fe63d447f13718c6a
BaltaX <kristian.sundstrom@agrifodd.lu.se>
2019-03-22 16:03:49 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
+//If consent box accepted, remove it and set cookie to 1 (=accepted)
 $("#accept-cookies").click(function () {
     $("#cookie-consent").css("display", "none");
+    //Calculate expiry date to 1 year from now
     var now = new Date();
     var time = now.getTime();
-    time += 30 * 1000;
+    time += 60 * 60 * 24 * 365 * 1000;
     now.setTime(time);
+    //Set cookie to 1 and add expiry date
     document.cookie = 'cookie-consent=1;expires=' + now.toUTCString();
 });
 
-
-console.log(document.cookie);
-
-//If cookie exists
+//If cookie does not exist/has the wrong value, display consent box
 if (getCookie("cookie-consent") != "1") {
     $("#cookie-consent").css("display", "block");
 }
 
-//Returns value of cookie if cookie 'name' exists, else returns null
+//Help function that returns value of cookie if cookie 'name' exists, else returns null
 function getCookie(name) {
     var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return v ? v[2] : null;
